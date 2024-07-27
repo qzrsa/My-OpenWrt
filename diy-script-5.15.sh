bdb1275d3b939a019c7b1f41493d00200cb5fe02
丶曲終人散ゞ <44892049+qzrsa@users.noreply.github.com>
2024-07-27 13:05:36 +0800

    Update diy-script-5.15.sh

--- diy-script-5.15.sh
+++ diy-script-5.15.sh
@@ -15,9 +15,10 @@ sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
 
 
 # 移除要替换的包
-rm -rf feeds/luci/applications/luci-app-mosdns
-rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,v2ray*,sing*,smartdns}
-rm -rf feeds/packages/utils/v2dat
+rm -rf feeds/smpackage/{base-files,dnsmasq,firewall*,fullconenat,libnftnl,nftables,ppp,opkg,ucl,upx,vsftpd*,miniupnpd-iptables,wireless-regdb}
+#rm -rf feeds/luci/applications/luci-app-mosdns
+#rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,v2ray*,sing*,smartdns}
+#rm -rf feeds/packages/utils/v2dat
 rm -rf feeds/packages/lang/golang
 rm -rf feeds/packages/net/mosdns
 rm -rf feeds/packages/net/msd_lite
