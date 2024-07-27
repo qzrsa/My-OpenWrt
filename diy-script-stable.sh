2621aab997898b5de2f57b0bb64e6d53b96fe28f
丶曲終人散ゞ <44892049+qzrsa@users.noreply.github.com>
2024-07-27 20:41:18 +0800

    Update diy-script-stable.sh

--- diy-script-stable.sh
+++ diy-script-stable.sh
@@ -48,12 +48,6 @@ git clone --depth=1 https://github.com/Jason6111/luci-app-netdata package/luci-a
 git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages/tree/patches-xray-core-1.8.21 package/openwrt-passwall
 git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall package/luci-app-passwall
 
-# MosDNS
-git clone --depth=1 https://github.com/sbwml/luci-app-mosdns package/luci-app-mosdns
-
-# Gowebdav
-git clone --depth=1 https://github.com/qzrsa/openwrt-packages package/luci-app-gowebdav
-
 # Themes
 git clone --depth=1 -b 18.06 https://github.com/kiddin9/luci-theme-edge package/luci-theme-edge
 git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
